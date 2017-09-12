2cbd62f1aebbfce40ee18377cd7ac472f7ab3bb4
Subhankarc79 <subhankar.chatterjee@cgi.com>
2017-09-12 16:42:02 -0600

    Upload Spring Project # 1

---
+++ mstest1/src/main/java/com/example/mstest1/Mstest1Application.java
@@ -0,0 +1,12 @@
+package com.example.mstest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mstest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mstest1Application.class, args);
+	}
+}
